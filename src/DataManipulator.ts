55524306a6fa43d7342a6071a18b47e4e751c47b
Jiacheng Ma <jiacheng.ma@stonybrook.edu>
2023-12-25 19:33:43 -0500

    Added thought process and comments

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,5 +1,7 @@
+// Import the ServerRespond type from the DataStreamer module.
 import { ServerRespond } from './DataStreamer';
 
+// Define the Row interface to structure the data for the visualization with new fields.
 export interface Row {
   price_abc: number;
   price_def: number;
@@ -10,24 +12,44 @@ export interface Row {
   trigger_alert: number | undefined;
 }
 
-
-
+// Create a class that will handle data manipulation logic.
 export class DataManipulator {
+  // Define a static method to generate a Row object from an array of ServerRespond objects.
   static generateRow(serverRespond: ServerRespond[]): Row {
+    // Calculate the average price for stock 'abc' by averaging the top ask and bid prices.
     const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    // Calculate the average price for stock 'def' in the same manner.
     const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+    // Calculate the ratio of the two average prices.
     const ratio = priceABC / priceDEF;
-    const upperBound = 1 + 0.10; //0.05
-    const lowerBound = 1 - 0.10; //0.05
+    // Adjust the upper and lower bounds for the ratio to be 10% above and below 1, which is a change from the previous 5%.
+    const upperBound = 1 + 0.10; // Increased from the initial 0.05
+    const lowerBound = 1 - 0.10; // Increased from the initial 0.05
+    // Return a new Row object with the calculated values and decide if a trigger alert should be activated based on the ratio.
     return {
       price_abc: priceABC,
       price_def: priceDEF,
       ratio,
+      // Choose the later timestamp between the two server responses.
       timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ? 
         serverRespond[0].timestamp : serverRespond[1].timestamp,
       upper_bound: upperBound,
       lower_bound: lowerBound,
+      // Set the trigger_alert to the ratio if it exceeds the bounds, otherwise leave it undefined.
       trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
     };
   }
 }
+/* In these updates to the DataManipulator.ts file, the thought process was to restructure the data into
+ a new Row interface that is better suited to the updated visualization needs. The focus is on calculating 
+ a price ratio and determining if this ratio falls outside of newly adjusted bounds, which have been expanded
+  from a 5% margin to a 10% margin around the value of 1. This change would allow for a broader range of 
+  fluctuation before triggering an alert. */
+
+/* After applying the changes from the patch file, the realization was that increasing the threshold for 
+the upper and lower bounds to 10% might make the bounds too large, potentially allowing for more significant 
+fluctuations in the ratio without triggering an alert. This could mean that smaller but potentially important
+ variations in the ratio might not be captured or highlighted, suggesting that a narrower margin might be more
+  sensitive and effective for certain analytical purposes. The process of adjusting these values and observing
+   their impact on the data visualization would have provided valuable insight into how to fine-tune such 
+   parameters for different analytical needs. */
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,9 +1,11 @@
+// Import necessary modules and styles. The inclusion of TableData suggests we need it for type checking or for using the Perspective table functionalities.
 import React, { Component } from 'react';
 import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
 
+// Define interfaces for props and for the custom PerspectiveViewerElement.
 interface IProps {
   data: ServerRespond[],
 }
@@ -11,17 +13,22 @@ interface IProps {
 interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
+
+// Define the Graph component which will render and manage the PerspectiveViewer for visualizing the data.
 class Graph extends Component<IProps, {}> {
   table: Table | undefined;
 
+  // Render method returns a PerspectiveViewer element, which will be manipulated in other lifecycle methods.
   render() {
     return React.createElement('perspective-viewer');
   }
 
+  // When the component mounts, we initialize the PerspectiveViewer and set up the table schema.
   componentDidMount() {
-    // Get element from the DOM.
+    // Acquire a reference to the PerspectiveViewer element in the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
+    // Define the schema for the data that will be visualized, reflecting the new data structure focusing on price ratios and alerts.
     const schema = {
       price_abc:'float',
       price_def:'float',
@@ -30,32 +37,35 @@ class Graph extends Component<IProps, {}> {
       upper_bound: 'float',
       lower_bound: 'float',
       trigger_alert: 'float',
-      
     };
 
+    // Initialize the Perspective table if possible.
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
     }
-    if (this.table) {
-      // Load the `table` in the `<perspective-viewer>` DOM reference.
-      elem.load(this.table);
-      elem.setAttribute('view', 'y_line');
-      elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
-      elem.setAttribute('aggregates', JSON.stringify({
-      price_abc: 'avg',
-      price_def: 'avg',
-      ratio: 'avg',
-      timestamp: 'distinct count',
-      upper_bound: 'avg',
-      lower_bound: 'avg',
-      trigger_alert: 'avg',
-}));
 
+    // If the table is initialized, configure the PerspectiveViewer with the appropriate attributes for the visualization.
+    if (this.table) {
+      elem.load(this.table); // Load the table into the PerspectiveViewer.
+      elem.setAttribute('view', 'y_line'); // Set the view type to a line chart, focusing on time series data.
+      elem.setAttribute('row-pivots', '["timestamp"]'); // Pivot the data by timestamp to track changes over time.
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]'); // Specify the columns to be visualized, focusing on the ratio and its bounds.
+      // Define how the data should be aggregated in the visualization.
+      elem.setAttribute('aggregates', JSON.stringify({
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
+        timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
+      }));
     }
   }
 
+  // When the component updates, update the Perspective table with new data.
   componentDidUpdate() {
+    // Check if the table exists and if there is new data, then update the table accordingly.
     if (this.table) {
       this.table.update([
         DataManipulator.generateRow(this.props.data),
@@ -64,4 +74,5 @@ class Graph extends Component<IProps, {}> {
   }
 }
 
+// Make the Graph component available for import into other modules.
 export default Graph;
